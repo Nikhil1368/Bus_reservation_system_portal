154fba4d2d655e8e9a4a1311b7aea73a4b37bde8
Nikhil1368 <avihaansharma1308@gmail.com>
2022-11-09 19:15:18 +0530

    added initial data

---
+++ Bus_Reservation_System_Portal/src/main/java/com/app/model/Reservation.java
@@ -0,0 +1,19 @@
+package com.app.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+	
+	private Integer reservationId;
+	private String reservationStatus;
+	private String reservationType;
+	private LocalDate reservationDate;
+	private LocalTime reservationTime;
+	private String source;
+	private String destination;
+	private Bus bus;
+	
+	
+
+}
