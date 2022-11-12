a9b37821cce96b2e0d2420b290ef87dc00532bbe
DheerajPandey1 <dheerajpandey071@gmail.com>
2022-11-12 14:21:02 +0530

    Busdataadded

--- Bus_Reservation_System_Portal/src/main/java/com/app/service/BusService.java
+++ Bus_Reservation_System_Portal/src/main/java/com/app/service/BusService.java
@@ -1,13 +1,14 @@
 package com.app.service;
 
+import com.app.exceptions.BusExseption;
 import com.app.model.Bus;
 
 public interface BusService {
 
-	public Bus addBus(Bus bus);
-	public Bus updateBus(Bus bus);
-	public Bus deleteBus(int busId);
-	public Bus viewBus(int busId);
+	public Bus addBus(Bus bus)throws BusExseption;
+	public Bus updateBus(Bus bus)throws BusExseption;
+	public Bus deleteBus(int busId)throws BusExseption;
+	public Bus viewBus(int busId)throws BusExseption;
 	
 	
 }
